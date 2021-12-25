a6e47513c0aae8c02ac933551afb64575779e0c9
zumbalove974 <omranedoo@gmail.com>
2021-12-25 21:44:51 +0100

    Liaison entre la page principale et terminus_schedules

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ function getInfos() {
     let station = document.getElementById("station").value;
     if(station){
         let script = document.createElement("script");
-        script.src = "js/terminus_schedules.js";
+        script.src = "../js/terminus_schedules.js";
         let body = document.getElementById("body");
         body.appendChild(script);
     }
